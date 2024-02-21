5cac2ab913846f2945c39d5aecf3273c3b19132e
Martin Dinahet <martin.dinahet@proton.me>
2024-02-21 19:19:34 +0100

    removed examples and (finally) didn't forget to add basic type aliases

--- main.ts
+++ main.ts
@@ -5,6 +5,20 @@ import compiler from "./src/compiler.ts";
 const source = Deno.readTextFileSync(Deno.args[0]);
 const tokens = new lexer(source).run();
 const astree = new parser(tokens).run();
-const output = compiler(astree) + "\nDeno.exit(main())";
+const output = compiler(astree) + generate_types() + generate_main();
+
+function generate_types(): string {
+  return `
+    type str = string
+    type num = number
+    type boo = boolean
+  `;
+}
+
+function generate_main(): string {
+  return `
+    Deno.exit(main())
+  `;
+}
 
 Deno.writeTextFileSync("./out/output.ts", output);
